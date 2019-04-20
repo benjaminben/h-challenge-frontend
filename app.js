9948ec2a5d530db097aefeeef958a2fb749098d7
benBen <benmbenjamin@gmail.com>
2019-04-20 07:10:02 -0500

    details (not responsive)

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 (function() {
-  console.log("ayy")
+  var cards = Array.from(document.querySelectorAll(".card"))
+  cards.forEach(function(c,i,a) {
+    c.querySelector(".stats").addEventListener("click", function(e) {
+      c.className = c.className.match(/ liked/)
+                  ? c.className.replace(/ liked/, "")
+                  : c.className + " liked"
+    })
+  })
 })()
